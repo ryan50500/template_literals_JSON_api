ccb86936664f978becb2d684c253fc0a582e9f9a
ryan woods <federer5050@gmail.com>
2020-11-24 14:28:46 +0000

    random meal updated on page load

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+const meals = document.getElementById('meals');
+
 getRandomMeal();
 
 async function getRandomMeal() {
     const resp = await fetch(
         "https://www.themealdb.com/api/json/v1/1/random.php"
-    ).data;
-    const randomMeal = await resp;
+    );
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0];
 
-    console.log(randomMeal);
+    addMeal(randomMeal, true);
 }
 
 async function getMealById(id) {
@@ -20,4 +23,31 @@ async function getMealsBySearch(term) {
     const meal = await fetch(
         "https://www.themealdb.com/api/json/v1/1/search.php?s=" + term
     );
+}
+
+function addMeal(mealData, random = false) {
+    const meal = document.createElement('div');
+    meal.classList.add('meal');
+
+    meal.innerHTML = `
+            <div class="meal-header">
+            ${
+                random
+                   ? `
+            <span class="random"> Random Recipe</span>`
+                    : ""
+            }
+                <img
+                    src="${mealData.strMealThumb}"
+                    alt="${mealData.strMeal}"
+                />
+            <div class="meal-body">
+                <h4>${mealData.strMeal}</h4>
+                <button class="fav-btn">
+                    <i class="fas fa-heart"></i>
+                    </button>
+            </div>
+    `;
+
+    meals.appendChild(meal);
 }
